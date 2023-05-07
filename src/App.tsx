41a0892f824a5cdd5e18307f43f8a248cf78086a
Tyler Monaghan <33797221+tymonaghan@users.noreply.github.com>
2023-05-07 21:27:49 +0000

    Implement Chakra UI Library

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import React from "react";
 import TestOne from "./TestOne";
+import {
+  Box,
+  Heading,
+  Text,
+  Image,
+  Center,
+  Stack,
+  Button,
+} from "@chakra-ui/react";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-      <TestOne planet="Mars" />
-    </div>
+    <Box>
+      <Center>
+        <Heading as="h1" size="2xl">
+          Trek Picker
+        </Heading>
+      </Center>
+      <Center>
+        <TestOne planet="Mars" />
+      </Center>
+      <Center>
+        <Image src="picard.jpg" />
+      </Center>
+      <Center>
+        <Stack direction={"row"} spacing={"2.5"}>
+          <Button colorScheme="blue">Red Pill</Button>
+          <Button colorScheme="red">Blue Pill</Button>
+        </Stack>
+      </Center>
+    </Box>
   );
 };
 
--- src/TestOne.tsx
+++ src/TestOne.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Badge } from "@chakra-ui/react";
 
-const TestOne = ({planet}) => {
+const TestOne = ({ planet }) => {
   return (
     <div>
-        <h2>Goodbye {planet}</h2>
+      <h2>
+        Goodbye <Badge variant={"subtle"}>{planet}</Badge>
+      </h2>
     </div>
   );
 };
