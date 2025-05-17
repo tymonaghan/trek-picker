00ce77f96d63fd077a7e507145f99d5aff7ff38f
Tyler Monaghan <33797221+tymonaghan@users.noreply.github.com>
2025-05-17 03:19:04 +0000

    Delete unneeded babel config

--- client/babel.config.js
+++
@@ -1,8 +0,0 @@
-// This file is no longer needed after migrating to Vite.
-
-export default {
-  presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }], // For ES modules
-    '@babel/preset-typescript', // For TypeScript
-  ],
-};
